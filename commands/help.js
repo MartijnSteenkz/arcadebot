d45b915718f87d63f102929595aa8fbbb6c75993
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2018-09-17 23:21:09 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,17 +5,17 @@ module.exports.run = async (bot, message, args) => {
   let helpembed = new Discord.RichEmbed()
   .setDescription("Command List")
   .setColor("#fa2a7d")
-  .addField("a!help", "shows a list of all the commands")
-  .addField("a!report @someone reason", "report someone in the server")
-  .addField("a!serverinfo", "info about the server")
-  .addField("a!botinfo", "info about the bot")
-  .addField("a!userinfo", "info about the user")
-  .addField("a!ping", "shows the response time of the bot")
+  .addField("a!help", "laat een lijst met alle commands zien")
+  .addField("a!report @someone reason", "report iemand in de server")
+  .addField("a!serverinfo", "informatie over de server")
+  .addField("a!botinfo", "informatie over de bott")
+  .addField("a!userinfo", "informatie over een user")
+  .addField("a!ping", "laat de reactie tijd zien van de bot")
   .addField("[staff only] a!addrole @someone role", "voeg een role aan iemand toe")
   .addField("[staff only] a!removerole @someone role", "verwijder een role van iemand")
   .addField("[staff only] a!tempmute @someone time", "mute iemand voor een bepaalde tijd")
-  .addField("[staff only] a!kick @someone reason", "kick a person from the server")
-  .addField("[staff only] a!ban @someone reason", "ban a person from the server");
+  .addField("[staff only] a!kick @someone reason", "kick iemand van de server")
+  .addField("[staff only] a!ban @someone reason", "ban iemand van de server");
 
   message.channel.send(helpembed);
 }
