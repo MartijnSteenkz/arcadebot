2e3f6ccd6c6543d63d7c92b770c60d7b0ceb1b14
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2018-09-23 14:56:13 +0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -18,7 +18,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("Region", message.guild.region)
         .addField("Total Roles", message.guild.roles.size)
         .addField("Total Channels", message.guild.channels.size)
-        .addField("Invite Link", "https://discord.gg/vGu2qEj");
+        .addField("Invite Link", "https://discord.gg/xj8VCV2");
 
         message.channel.send(serverembed);
 
