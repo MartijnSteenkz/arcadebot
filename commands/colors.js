0818ecb70c554bc20cbe2b55bd7bda1ee38ca5be
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-19 13:42:24 +0100

    Update colors.js

--- commands/colors.js
+++ commands/colors.js
@@ -1,17 +1,17 @@
-const Discord = require("discord.js")
-const superagent = require("superagent");
+//const Discord = require("discord.js")
+//const superagent = require("superagent");
 
-module.exports.run = async (bot,messages,args) => {
+//module.exports.run = async (bot,messages,args) => {
 
-  let sicon = messages.guild.iconURL;
-  let colorEmbed = new Discord.RichEmbed()
-  .setColor("#da3681")
-  .setThumbnail(sicon)
-  .addField("Color Roles", "CyanColor\nLightBlueColor\nBlueColor\nDarkBlueColor\nLightGreenColor\nGreenColor\nDarkGreenColor\nBloodRedColor\nRedColor\nDarkRedColor\nSweetPurpleColor\nPurpleColor\nPinkColor\nBurntOrangeColor\nOrangeColor\nYellowColor\nGreyColor\nBlackColor\nWhiteColor")
-  .setFooter("To add a color, use the command h!add {color}");
+//  let sicon = messages.guild.iconURL;
+//  let colorEmbed = new Discord.RichEmbed()
+//  .setColor("#da3681")
+//  .setThumbnail(sicon)
+//    .addField("Color Roles", "CyanColor\nLightBlueColor\nBlueColor\nDarkBlueColor\nLightGreenColor\nGreenColor\nDarkGreenColor\nBloodRedColor\nRedColor\nDarkRedColor\nSweetPurpleColor\nPurpleColor\nPinkColor\nBurntOrangeColor\nOrangeColor\nYellowColor\nGreyColor\nBlackColor\nWhiteColor")
+//    .setFooter("To add a color, use the command h!add {color}");
 
-  messages.channel.send(colorEmbed);
-}
-module.exports.help = {
-  name: "colors"
-}
+//  messages.channel.send(colorEmbed);
+//}
+//module.exports.help = {
+//  name: "colors"
+//}
