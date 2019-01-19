68f66809006941ff23640bb98f00f8edd0f81cb6
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-19 12:57:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,28 @@ bot.on("ready", async () => {
 
   //bot.user.setGame("DGC Original Bot");
 });
+bot.on("message", async message => {
+
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: botconfig.prefix
+    };
+  }
+  let prefix = prefixes[message.guild.id].prefixes;
+
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (!message.content.startsWith(prefix)) return;
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+  });
 
 bot.on("message", async message => {
   if(message.author.bot) return;
