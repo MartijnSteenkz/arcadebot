77eaf189afaf62a43902630d0cecc5f9daec759a
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-19 13:26:57 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -5,12 +5,13 @@ module.exports.run = async (bot, message, args) => {
   let uicon = member.user.displayAvatarURL
   let userembed = new Discord.RichEmbed()
   .setDescription("User Information")
-  .setColor("#fa2a7d")
+  .setColor("#da3681")
   .setThumbnail(uicon)
-  .addField("Full Username", `${message.author.tag}`)
-  .addField("Nickname", message.member.displayName)
+  .addField("Full Username", `${message.author.tag}`, true)
+  .addField("Nickname", message.member.displayName, true)
   .addField("User ID", message.author.id)
-  .addField("Created At", message.author.createdAt);
+  .addField("Created At", message.author.createdAt)
+  .addField("Joined At", message.member.joinedAt);
 
   message.channel.send(userembed);
 
