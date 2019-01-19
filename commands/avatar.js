bd4891b72016bb01b5ab73efe3d5fe07b11f6694
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-19 13:00:42 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,15 +4,15 @@ module.exports.run = async (bot, message, args) => {
 
  let aTaged = message.mentions.users.first();
  let noTagAvatarEmbed = new Discord.RichEmbed()
- .setDescription(`Avatar of ${message.author.username}`)
- .setColor("#af0e97")
+ .setDescription(`Avatar van ${message.author.username}`)
+ .setColor("#da3681")
  .setImage(`${message.author.displayAvatarURL}`);
 
  if(!args[0]) return message.channel.send(noTagAvatarEmbed);
 
  let avatarEmbed = new Discord.RichEmbed()
- .setDescription(`Avatar of ${aTaged.username}`)
- .setColor("#af0e97")
+ .setDescription(`Avatar van ${aTaged.username}`)
+ .setColor("#da3681")
  .setImage(`${aTaged.displayAvatarURL}`);
 
  message.channel.send(avatarEmbed);
