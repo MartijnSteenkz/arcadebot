841a2cde9286cf871a3c66032c19f44876c442f7
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-19 12:30:44 +0100

    Add files via upload

---
+++ commands/doggo.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+const superagent = require("superagent");
+
+module.exports.run = async (bot,messages,args) => {
+
+  let response = await superagent.get(`https://random.dog/woof.json`);
+  let dogembed = new Discord.RichEmbed()
+
+  .setColor("#af0e97")
+  .setTitle("Doggo :dog:")
+  .setImage(response.body.url);
+
+  messages.channel.send(dogembed);
+}
+module.exports.help = {
+  name: "doggo"
+}
