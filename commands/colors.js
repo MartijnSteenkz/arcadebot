bc65bcadfd9aee04eec483b37f8fe00578773530
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-19 12:50:34 +0100

    Update colors.js

--- commands/colors.js
+++ commands/colors.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot,messages,args) => {
   let colorEmbed = new Discord.RichEmbed()
   .setColor("#af0e97")
   .setThumbnail(sicon)
-  .addField("Color Roles", "CyanColor\nLightBlueColor\nBlueColor\nDarkBlueColor\nLightGreenColor\nGreenColor\nDarkGreenColor\nBloodRedColor\nRedColor\nDarkRedColor\nSweetPurpleColor\nPurpleColor\nPinkColor\nBurntOrangeColor\nOrangeColor\nYellowColor\nGreyColor\nBlackColor")
+  .addField("Color Roles", "CyanColor\nLightBlueColor\nBlueColor\nDarkBlueColor\nLightGreenColor\nGreenColor\nDarkGreenColor\nBloodRedColor\nRedColor\nDarkRedColor\nSweetPurpleColor\nPurpleColor\nPinkColor\nBurntOrangeColor\nOrangeColor\nYellowColor\nGreyColor\nBlackColor\nWhiteColor")
   .setFooter("To add a color, use the command h!add {color}");
 
   messages.channel.send(colorEmbed);
