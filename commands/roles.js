77b27d73aafee2184a9da3ecd40be7eccc856a15
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-19 12:52:45 +0100

    Update roles.js

--- commands/roles.js
+++ commands/roles.js
@@ -8,9 +8,8 @@ module.exports.run = async (bot,messages,args) => {
   .setTitle("Extra Roles")
   .setColor("#af0e97")
   .setThumbnail(sicon)
-  .addField("Info", "Male\nFemale\nTransgender\nStraight\nHomosexual\nBisexual\nAsexual\nSingle\nTaken\nDom\nSub\nSwitch\nLevel Block", true)
-  .addField("Likes", "Vanilla\nFetish\nAhegao\nFuta\nTentacles\nNeko\nLoli\nThicc\nBDSM\nFurry\nYuri\nYaoi", true)
-  .setFooter("To add a role, use the command h!add {role}");
+  .addField("Info", "Level Block")
+  .setFooter("To add a role, use the command a!add {role}");
 
   messages.channel.send(rolesEmbed);
 }
