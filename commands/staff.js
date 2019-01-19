f3039f00684ad13f12aa90333d373334a5ab3e38
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-19 13:24:13 +0100

    Update staff.js

--- commands/staff.js
+++ commands/staff.js
@@ -3,12 +3,12 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   let staffembed = new Discord.RichEmbed()
-  .setDescription("All Current Staff Members")
+  .setDescription("Alle Staff members")
   .setColor("#af0e97")
   .setThumbnail(message.guild.iconURL)
   .addField("Owner", `<@246693689731186688>`)
-  .addField("Admin", `<@377284513723842561>`)
-  .addField("Mods", `<@253227808208191488> <@302469215779160065> <@203471080742977538>`);
+  .addField("Admin", `<@218137001155624960> <@180324781172719616>`)
+  .addField("Mods", `<@197344574471929856> <@232221728619626496>`);
 
   message.channel.send(staffembed);
 
