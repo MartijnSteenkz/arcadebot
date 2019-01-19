e9407605b7a20638036d68f3dbeb1ab429d6d5cd
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-19 13:20:52 +0100

    Update roles.js

--- commands/roles.js
+++ commands/roles.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot,messages,args) => {
   let sicon = messages.guild.iconURL;
   let rolesEmbed = new Discord.RichEmbed()
   .setTitle("Extra Roles")
-  .setColor("#af0e97")
+  .setColor("#da3681")
   .setThumbnail(sicon)
   .addField("Info", "Level Block")
   .setFooter("To add a role, use the command a!add {role}");
