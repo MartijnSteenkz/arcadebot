04f1ec7b77d4a7b11a8ef9ef1aea1b7948189505
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-19 12:30:29 +0100

    Add files via upload

---
+++ commands/colors.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+const superagent = require("superagent");
+
+module.exports.run = async (bot,messages,args) => {
+
+  let sicon = messages.guild.iconURL;
+  let colorEmbed = new Discord.RichEmbed()
+  .setColor("#af0e97")
+  .setThumbnail(sicon)
+  .addField("Color Roles", "CyanColor\nLightBlueColor\nBlueColor\nDarkBlueColor\nLightGreenColor\nGreenColor\nDarkGreenColor\nBloodRedColor\nRedColor\nDarkRedColor\nSweetPurpleColor\nPurpleColor\nPinkColor\nBurntOrangeColor\nOrangeColor\nYellowColor\nGreyColor\nBlackColor")
+  .setFooter("To add a color, use the command h!add {color}");
+
+  messages.channel.send(colorEmbed);
+}
+module.exports.help = {
+  name: "colors"
+}
