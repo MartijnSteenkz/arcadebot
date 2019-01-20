09fd6c11c74900944eac7d04e63ba49cbb31d551
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-20 21:36:05 +0100

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -6,20 +6,17 @@ module.exports.run = async (bot,message,args) => {
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("Can't find user");
   let role = args.join(" ").slice(22);
-  if(!role) return message.reply("That role doesn't exist");
   let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.reply("Cannot find that role");
 
-  if(rMember.roles.has(gRole.id)) return message.reply("Ze hebben al die role");
   await(rMember.addRole(gRole.id));
 
 try{
-  await rMember.send(`Ayy, you got the ${gRole.name} role, congrats brother!`)
+  await rMember.send(`Your report has been declined. Thank you for reporting. Please check your report to see the reason why it was declined.`)
 }catch(e){
-  message.channel.send(`${gRole.name} toegevoegd aan <@${rMember.id}>`)
+  message.channel.send(`Report declined from <@${rMember.id}>`)
   }
 }
 
 module.exports.help = {
-  name: "addrole"
+  name: "decline"
 }
