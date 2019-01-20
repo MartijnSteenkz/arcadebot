75daa3ac97ea1d46d5095f600b78ea695822c843
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-20 19:45:16 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, messages, args) => {
 
-  if(!messages.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Fuck off");
-  if(!args[0]) return messages.channel.send("Fuck off will ya?");
+  if(!messages.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Euhh no brother.");
+  if(!args[0]) return messages.channel.send("You maybe wanna eh, select a number to delete sir");
   let clearEmbed = new Discord.RichEmbed()
     .setColor("#7493e4")
     .setTitle(`I deleted ${args[0]} for you! Now where's my reward money?`);
