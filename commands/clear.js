ad3a5ecd269bd886fdd6906120aec412f545f8df
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-20 19:44:09 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot, messages, args) => {
   if(!messages.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Fuck off");
   if(!args[0]) return messages.channel.send("Fuck off will ya?");
   let clearEmbed = new Discord.RichEmbed()
-    .setColor("#da3681")
-    .setTitle(`${args[0]} berichten verwijderd!`);
+    .setColor("#7493e4")
+    .setTitle(`I deleted ${args[0]} for you! Now where's my reward money?`);
 
   messages.channel.bulkDelete(args[0]).then(() => {
     messages.channel.send(clearEmbed).then(msg => msg.delete(5000));
