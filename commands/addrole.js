408d852dc060625867280a6d54df6c3a664f482e
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-20 19:31:15 +0100

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -2,19 +2,19 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry my dude, je kan dat niet zomaar doen");
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("HAH, you thought! Get outa here");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember) return message.reply("Kan de user niet vinden");
+  if(!rMember) return message.reply("Can't find user");
   let role = args.join(" ").slice(22);
-  if(!role) return message.reply("Dat is geen role");
+  if(!role) return message.reply("That role doesn't exist");
   let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.reply("Kon die role niet vinden");
+  if(!gRole) return message.reply("Cannot find that role");
 
   if(rMember.roles.has(gRole.id)) return message.reply("Ze hebben al die role");
   await(rMember.addRole(gRole.id));
 
 try{
-  await rMember.send(`Ayy, je kreeg de rol ${gRole.name}`)
+  await rMember.send(`Ayy, you got the ${gRole.name} role, congrats brother!`)
 }catch(e){
   message.channel.send(`${gRole.name} toegevoegd aan <@${rMember.id}>`)
   }
