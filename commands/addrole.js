fac7d1c73204a6a072ebd16b09d894e1847ef5da
Steenkzzisback <32714415+MartijnSteenkz@users.noreply.github.com>
2019-01-20 21:38:40 +0100

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -5,10 +5,6 @@ module.exports.run = async (bot,message,args) => {
   if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("HAH, you thought! Get outa here");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("Can't find user");
-  let role = args.join(" ").slice(22);
-  let gRole = message.guild.roles.find(`name`, role);
-
-  await(rMember.addRole(gRole.id));
 
 try{
   await rMember.send(`Your report has been declined. Thank you for reporting. Please check your report to see the reason why it was declined.`)
